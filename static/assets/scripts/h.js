18bcc16b096560ed0e361c6e77661f2fc9e8a8bc
reallynice123 <148382740+reallynice123@users.noreply.github.com>
2024-04-21 12:39:32 -0400

    Update h.js

--- static/assets/scripts/h.js
+++ static/assets/scripts/h.js
@@ -32,7 +32,7 @@ if (!inFrame && !navigator.userAgent.includes('Firefox')) {
     doc.head.appendChild(link)
     doc.body.appendChild(iframe)
 
-    const pLink = localStorage.getItem(encodeURI('pLink')) || 'https://www.nasa.gov/'
+    const pLink = localStorage.getItem(encodeURI('pLink')) || 'https://www.google.com/'
     location.replace(pLink)
 
     const script = doc.createElement('script')
